eb50854b1d470c64a680dd94651ebabb8b13b066
carmenmaymo <mayen22@gmail.com>
2019-10-03 13:12:49 +0200

    Added selectPersonById feature

--- src/main/java/com/example/demo/dao/PersonDAO.java
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDAO {
@@ -13,4 +14,12 @@ public interface PersonDAO {
         return insertPerson(id,person);
     }
     List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID  id);
+
+    int updatePersonById(UUID id, Person person);
+
+
 }
